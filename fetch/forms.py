5d4c0fbdbc63f7c37250c68a978480c1d590dec3
asiandriver28 <63335665+asiandriver28@users.noreply.github.com>
2025-05-09 17:43:28 +0800

    rebasing

--- fetch/forms.py
+++ fetch/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, BooleanField, SubmitField, PasswordField, StringField
+<<<<<<< HEAD
 from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
@@ -11,3 +12,11 @@ class SignupForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField('Sign up')
+=======
+from wtforms.validators import DataRequired, min_length
+
+class LoginForm(FlaskForm):
+    username = StringField('Student ID', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), min_length(8)])
+    submit = SubmitField('Sign in')
+>>>>>>> abeb8ee (created a forms file to handle login and sign ups)
