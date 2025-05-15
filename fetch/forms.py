17696517d17a2345083f3fe2827ed98f0ed30e7c
asiandriver28 <63335665+asiandriver28@users.noreply.github.com>
2025-05-15 13:55:43 +0800

    added a form that connects the backend of the settings page allowing users to logout, close account and change their information stored on the database

--- fetch/forms.py
+++ fetch/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, BooleanField, SubmitField, PasswordField, StringField
-from wtforms.validators import DataRequired, Length
+from wtforms import IntegerField, BooleanField, SubmitField, PasswordField, StringField, SelectField
+from wtforms.validators import DataRequired, Length, Optional
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
@@ -12,3 +12,14 @@ class SignupForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     player_id = StringField('Player ID', validators=[DataRequired()])
     submit = SubmitField('Sign up')
+
+class SettingsForm(FlaskForm):
+    new_username = StringField("New Username", validators=[Optional(), Length(min=3, max=80)])
+    new_password = PasswordField("New Password", validators=[Optional(), Length(min=8)])
+    new_playerID = StringField("New Player ID", validators=[Optional(), Length(9)])
+    data_sharing = SelectField("Share my data with friends", choices=[("yes", "Yes"), ("no", "No")])
+    restricted_friends = StringField("Restricted Friends", validators=[Optional()])
+    
+    submit = SubmitField("Save Changes")
+    logout = SubmitField("Logout")
+    close_account = SubmitField("Close Account")
